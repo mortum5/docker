0cfe64e45bc836ae315b4961a2c84c49d6d29ac6
Andrey Gonchar <jeron312@gmail.com>
2019-05-14 22:45:05 +0000

    change app file

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN apt-get update && apt-get install -y swig gcc git libpulse-dev \
 libasound2-dev wget && \ 
 rm -rf /var/lib/apt/lists/* && \
 pip install --upgrade pip -r requirements.txt
-RUN wget https://de.ifmo.ru/--openedu/dataprocessing/appliedai/audio5/1.wav
 
+ENTRYPOINT ["args.sh"]
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,31 +1,42 @@
 import speech_recognition as sr
 import csv
+import wget
 
 # obtain path to "1.wav" in the same folder as this script
 from os import path
 
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "1.wav")
+with open('input.txt','r') as f:
+    v = f.readline()
+
+l = list(map(lambda x : int(x), v.split()))
 
 
-# use the audio file as the audio source
-r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
-        audio = r.record(source)  # read the entire audio file
+for i in l:
+    url = "https://de.ifmo.ru/--openedu/dataprocessing/appliedai/audio5/{}.wav".format(i)
+    wget.download(url)
 
-# recognize speech using Sphinx
-try:
-        print("Sphinx: " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
-except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
+    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "{}.wav".format(i))
 
-# recognize speech using Google Speech Recognition
+    # use the audio file as the audio source
+    r = sr.Recognizer()
+    with sr.AudioFile(AUDIO_FILE) as source:
+            audio = r.record(source)  # read the entire audio file
 
-try:
-        print("Google: " + r.recognize_google(audio))
-except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service;
-        {0}".format(e))
+    print("\nUsing {}.wav".format(i))
+    # recognize speech using Sphinx
+    try:
+            print("Sphinx: " + r.recognize_sphinx(audio))
+    except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+
+    # recognize speech using Google Speech Recognition
+
+    try:
+            print("Google: " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service;{0}".format(e))
+    print("\n")
